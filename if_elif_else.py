402cd730e91f696b6ae895d480d3d2c6823116de
DESKTOP-GV0R6HM\User <smita23tiwari.007@gmail.com>
2022-10-13 08:31:05 +0530

    +ve,-ve or 0

---
+++ if_elif_else.py
@@ -0,0 +1,8 @@
+#checking the given number is positive,negative or zero.
+num = -345
+if num > 0 :
+    print("the number is positive")
+elif num== 0:
+    print("the number is zero")
+else :
+    print ("the number is negative")
