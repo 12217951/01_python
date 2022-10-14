0fb7267b13326a3f937583b711b86c957337fc9b
DESKTOP-GV0R6HM\User <smita23tiwari.007@gmail.com>
2022-10-14 08:12:44 +0530

    assign value to variable

---
+++ assingnValueOfVariable.py
@@ -0,0 +1,15 @@
+#create the favorite_animal variable
+from xmlrpc.server import DocCGIXMLRPCRequestHandler
+
+
+favorite_animal = "tiger"
+print(favorite_animal)
+
+#creat the favorite_pet_animal vaariable
+favorite_pet_animal= "dog"
+
+#assign favorite_pet_animal to favorite_animal
+favorite_pet_animal = favorite_animal
+
+#print the favorite_animal variable
+print(favorite_animal)
