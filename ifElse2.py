9f2b24af233e8783d9af925af993d83880029284
DESKTOP-GV0R6HM\User <smita23tiwari.007@gmail.com>
2022-10-16 00:15:48 +0530

    ifElse2

---
+++ ifElse2.py
@@ -0,0 +1,6 @@
+#TAKE USER INPUT AND CHECK DIVISIBILITY BY BOTH 2 AND 3
+x=int(input("enter the number:"))
+if x/2 and x/3:
+    print("number is divisible by 2 and 3")
+else:
+    print("not divisible")
